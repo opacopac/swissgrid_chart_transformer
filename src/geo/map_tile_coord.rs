b420cb4560085b458446fba002ebeb2ab951ba9a
opacopac <armand@tschanz.com>
2022-03-29 23:56:15 +0200

    map tile coordinates

---
+++ src/geo/map_tile_coord.rs
@@ -0,0 +1,37 @@
+use std::f32::consts::PI;
+use std::fmt;
+use crate::Position2d;
+
+
+pub struct MapTileCoord {
+    pub x_tile: u32,
+    pub y_tile: u32,
+    pub zoom: u32
+}
+
+
+impl fmt::Display for MapTileCoord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}, y: {}, z: {}", self.x_tile, self.y_tile, self.zoom)
+    }
+}
+
+
+impl MapTileCoord {
+    pub fn from_position(pos: Position2d, zoom: u32) -> MapTileCoord {
+        let pow = 2_u32.pow(zoom) as f32;
+        let x_tile = ((pos.lon + 180.0) / 360.0 * pow).floor() as u32;
+        let y_tile = ((1.0 - (pos.lat.to_radians().tan() + 1.0 / pos.lat.to_radians().cos()).ln() / PI) / 2.0 * pow).floor() as u32;
+
+        return MapTileCoord { x_tile, y_tile, zoom };
+    }
+
+
+    pub fn to_position(&self) -> Position2d {
+        let n = 2_u32.pow(self.zoom) as f32;
+        let lon = self.x_tile as f32 / n * 360.0 - 180.0;
+        let lat = (((PI * (1.0 - 2.0 * self.y_tile as f32 / n)).sinh()).atan()).to_degrees();
+
+        return Position2d { lon, lat }
+    }
+}
--- src/geo/mod.rs
+++ src/geo/mod.rs
@@ -1,3 +1,4 @@
 pub mod position_2d;
 pub mod ch_1903_coord;
 pub mod extent_2d;
+pub mod map_tile_coord;
